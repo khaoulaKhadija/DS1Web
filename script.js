20111be1b07b62b58dc37eeaca2dbcb91a902ba7
Client13@EST.LAN <Client13@EST.LAN>
2019-11-05 16:27:17 +0000

    ds

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//KANBOUA KHAOULA M'GUIGUIZE KHADIJA
+
+
 var s=document.getElementById('score-0');
 var s1 = document.getElementById("score-1");
 var s2 = document.getElementById("courent-0");
@@ -17,11 +20,23 @@ s2.innerHTML=0;
 s3.innerHTML=0;
   }
 
-
 }
-function cacher("de") {
+
+function actif(){
+	var a=document.querySelector(".panel-joueur-0");
+var b =document.querySelector(".panel-joueur-1 actif");
+	if(a.classList.contains("panel-joueur-0")){
+		a.classList.remove("panel-joueur-0");
+		a.classList.toggle("panel-joueur-1 actif");
+	}
+}
+
+
+
+function cacher() {
 
 var a=document.getElementsByClassName("de");
 for (var i = 0  ; i<a.length; i++) {
 	a[i].style.display="none"
 }}
+
